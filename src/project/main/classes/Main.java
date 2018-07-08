9b5b67e83b3839565c76fe7cfd9f78bc1660fc96
Tudor <cgabriela22001@gmail.com>
2018-07-09 01:25:47 +0300

    test

--- src/project/main/classes/Main.java
+++ src/project/main/classes/Main.java
@@ -39,8 +39,7 @@ public class Main {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Drone Alpha v0.1");
-        //baws
-	
+        //todo: create a connectiion with the arduino
 	}
 
 }
