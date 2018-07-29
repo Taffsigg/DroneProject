ac0f36dc31300d2d6fa2b2864438aac5522ed351
Tudor <cgabriela22001@gmail.com>
2018-07-29 23:34:38 +0300

    added controller monitoring w GUI.

--- src/project/main/classes/DroneControl.java
+++ src/project/main/classes/DroneControl.java
@@ -18,8 +18,11 @@ import org.lwjgl.input.Controllers;
 
 import com.fazecast.jSerialComm.SerialPort;
 import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+import javax.swing.JToggleButton;
 
-public class Main {
+public class DroneControl {
 	String s;
 	private JComboBox<String> comboBox;
 	private JFrame frame;
@@ -31,7 +34,11 @@ public class Main {
 	private JButton btnNewButton;
 	static Controller controller;
 	private JTextField textField;
-	
+	JToggleButton tglbtnNewToggleButton;
+	JSlider slider = new JSlider();
+	JSlider slider_1 = new JSlider();
+	JSlider slider_2 = new JSlider();
+	JSlider slider_3 = new JSlider();
 
 	/**
 	 * Launch the application.
@@ -40,7 +47,7 @@ public class Main {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Main window = new Main();
+					DroneControl window = new DroneControl();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -52,7 +59,7 @@ public class Main {
 	/**
 	 * Create the application.
 	 */
-	public Main() {
+	public DroneControl() {
 		initialize();
 	}
 
@@ -71,26 +78,31 @@ public class Main {
 		comboBox = new JComboBox<String>();
 		comboBox.setBounds(10, 11, 297, 20);
 		panel.add(comboBox);
-		
+
 		try {
 			Controllers.create();
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		Controllers.poll();
-		
-		
-		
-		for(int i=0; i<Controllers.getControllerCount();i++){
-			controller =  Controllers.getController(i);
-			System.out.println(controller.getName()+ " : " + controller.getIndex());
-		}
-		
-		controller = Controllers.getController(7);
 
-		
-		
+		Controllers.poll();
+
+		Controller cont = null;
+
+		for (int i = 0; i < Controllers.getControllerCount(); i++) {
+			controller = Controllers.getController(i);
+			System.out.println(controller.getName() + " : " + controller.getIndex());
+			if (controller.getName().equals("Mad Catz V.1 Stick")) {
+				cont = Controllers.getController(i);
+				for (int i1 = 0; i1 < cont.getButtonCount(); i1++) {
+					System.out.println(cont.getButtonName(i1) + ": " + cont.isButtonPressed(i1));
+
+				}
+			}
+		}
+
+		controller = cont;
+
 		SerialPort[] portNames = SerialPort.getCommPorts();
 		for (int i = 0; i < portNames.length; i++)
 			comboBox.addItem(portNames[i].getSystemPortName());
@@ -129,14 +141,14 @@ public class Main {
 		});
 		btnConnect.setBounds(10, 42, 89, 23);
 		panel.add(btnConnect);
-		
+
 		btnNewButton = new JButton("LED ON");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(btnNewButton.getText().equals("LED ON")) {
+				if (btnNewButton.getText().equals("LED ON")) {
 					data = "on";
 					btnNewButton.setText("LED OFF");
-				}else{
+				} else {
 					data = "off";
 					btnNewButton.setText("LED ON");
 				}
@@ -144,13 +156,55 @@ public class Main {
 		});
 		btnNewButton.setBounds(10, 80, 297, 23);
 		panel.add(btnNewButton);
-		
+
 		textField = new JTextField();
 		textField.setBounds(10, 114, 583, 20);
 		panel.add(textField);
 		textField.setColumns(10);
-		
-		
+
+		JLabel lblController = new JLabel("Controller :");
+		lblController.setBounds(10, 162, 123, 14);
+		panel.add(lblController);
+
+		slider.setValue(100);
+		slider.setMaximum(200);
+		slider.setBounds(86, 214, 200, 26);
+		panel.add(slider);
+
+		slider_1.setValue(100);
+		slider_1.setMaximum(200);
+		slider_1.setBounds(86, 262, 200, 26);
+		panel.add(slider_1);
+
+		slider_2.setValue(100);
+		slider_2.setMaximum(200);
+		slider_2.setBounds(86, 315, 200, 26);
+		panel.add(slider_2);
+
+		slider_3.setValue(100);
+		slider_3.setMaximum(200);
+		slider_3.setBounds(86, 366, 200, 26);
+		panel.add(slider_3);
+
+		JLabel lblThrottle = new JLabel("Throttle: ");
+		lblThrottle.setBounds(10, 226, 66, 14);
+		panel.add(lblThrottle);
+
+		JLabel lblYaw = new JLabel("Yaw: ");
+		lblYaw.setBounds(10, 262, 66, 14);
+		panel.add(lblYaw);
+
+		JLabel lblRoll = new JLabel("Roll: ");
+		lblRoll.setBounds(10, 315, 66, 14);
+		panel.add(lblRoll);
+
+		JLabel lblPitch = new JLabel("Pitch: ");
+		lblPitch.setBounds(10, 366, 66, 14);
+		panel.add(lblPitch);
+
+		tglbtnNewToggleButton = new JToggleButton("Start");
+		tglbtnNewToggleButton.setBounds(86, 416, 200, 23);
+		panel.add(tglbtnNewToggleButton);
 
 	}
 
@@ -171,25 +225,51 @@ public class Main {
 
 				sending = true;
 
+				boolean startdrone = false;
+				String sd = "0";
+
 				while (sending) {
-//					System.out.println("data is: " + data);
-					 
-					System.out.println("X : "+controller.getXAxisValue());
-					System.out.println("Y : "+controller.getYAxisValue());
-					System.out.println("Z : "+controller.getZAxisValue());
-					
-					textField.setText("X : "+controller.getXAxisValue()+ "   "+"Y : "+controller.getYAxisValue()+"   "+"Z : "+controller.getZAxisValue());
-					
-					controller.poll();
-					
-					if(data != null){
-					output.print(data.toString());
-					output.flush();
-					try {
-						Thread.sleep(100);
-					} catch (Exception e) {
+					// System.out.println("data is: " + data);
+
+					if (controller.isButtonPressed(6)) {
+						System.out.println("pressed");
+						if (!startdrone) {
+							startdrone = true;
+							sd = "1";
+							tglbtnNewToggleButton.setSelected(true);
+						} else {
+							startdrone = false;
+							sd = "0";
+							tglbtnNewToggleButton.setSelected(false);
+						}
+
 					}
-					System.out.println("Successfully Sending!");
+
+
+					String x = String.format("%.2f", controller.getXAxisValue() + 1);
+					String y = String.format("%.2f", controller.getYAxisValue() + 1);
+					String z = String.format("%.2f", controller.getZAxisValue() + 1);
+					String rz = String.format("%.2f", controller.getRZAxisValue() + 1);
+
+					slider_3.setValue(Math.round((controller.getYAxisValue() + 1) * 100));
+					slider_2.setValue(Math.round((controller.getXAxisValue() + 1) * 100));
+					slider_1.setValue(Math.round((controller.getRZAxisValue() + 1) * 100));
+					slider.setValue(Math.round((controller.getZAxisValue() + 1) * 100));
+
+					data = x + " " + y + " " + z;
+					textField.setText("X : " + x + "   " + "Y : " + y + "   " + "Z : " + z + " RZ : " + rz
+							+ " startdrone : " + sd);
+
+					controller.poll();
+
+					if (data != null) {
+						output.print(data.toString());
+						output.flush();
+						try {
+							Thread.sleep(100);
+						} catch (Exception e) {
+						}
+						System.out.println("Successfully Sending! " + data);
 					}
 				}
 			}
